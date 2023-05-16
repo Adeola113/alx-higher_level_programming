9015909c84cc442633dca02b3765ea3ffab7d32c
Adeola113 <adeolabello537@gmail.com>
2023-05-16 21:35:16 +0000

    task update

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -2,5 +2,5 @@
 -- from the database hbtn_0c_0
 -- results display score and name
 -- ordered by score
-with score >= 10
-SELECT score, name from second_table WHERE score >= 10 ORDER BY score DESC;
+-- with score >= 10
+SELECT score, name FROM  second_table WHERE score >= 10 ORDER BY score DESC;
