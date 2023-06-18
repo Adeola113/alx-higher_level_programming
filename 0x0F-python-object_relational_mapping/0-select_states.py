7451b68c36c4ad90baefa4525f3e1e72d3fd0842
Adeola113 <adeolabello537@gmail.com>
2023-06-18 19:29:30 +0000

    task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,15 +5,14 @@ database `hbtn_0e_0_usa`.
 """
 
 import MySQLdb
-import sys import argv
+from sys import argv
 
 if __name__ == '__main__':
     """
     Access to the database and get the states
     from the database.
     """
-
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
 
      cur = db.cursor()
