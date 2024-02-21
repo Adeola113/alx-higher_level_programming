7249aa9ae3cc77269805f2daea74cf0d1950b0a3
Adeola113 <adeolabello537@gmail.com>
2024-02-20 19:13:50 -0800

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-const fs = require('fs');
-const file = process.argv[2];
+const fs = require('fs')
+cons file = process.argv[2];
 
 fs.readFile(file, 'utf-8', function (err, data) {
   if (err) {
-    console.log(err);
+    console.log(data);
   } else {
     console.log(data);
   }
